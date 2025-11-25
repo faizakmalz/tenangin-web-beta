f6cfe2047ce77680495c0ddaeb152e293df53ae5
Faza <fakmalzaidaan@gmail.com>
2025-11-25 21:42:51 +0700

    rebuild

---
+++ app/(protected)/announcements/[id]/page.tsx
@@ -0,0 +1,17 @@
+// app/(protected)/announcements/[id]/page.tsx
+import { FC } from 'react';
+
+interface AnnouncementProps {
+  params: { id: string };
+}
+
+const AnnouncementPage: FC<AnnouncementProps> = ({ params }) => {
+  return (
+    <div>
+      <h1>Announcement {params.id}</h1>
+      {/* Konten pengumuman berdasarkan ID */}
+    </div>
+  );
+};
+
+export default AnnouncementPage;
